5929dcd6eb4b689728fcf948d96cb376906a26db
Youn Hee Pernling Frodin <yonipernling@gmail.com>
2021-04-26 16:04:43 -0400

    js added for accordion on about faq page

--- js/main-script.js
+++ js/main-script.js
@@ -8,3 +8,22 @@ function myFunction() {
 	}
 }
 
+/* About FAQ accordion */
+window.onload= function() { /* makes this js run after the html is loaded */
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+  	/* add and remove active class */
+    this.classList.toggle("active");
+    /* hide and show active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+}
